236fc3add8ad2c9d643617fa28e1fbf8975ac8e1
saido <38206432+JSaii@users.noreply.github.com>
2025-06-16 12:58:27 +0900

    Add getHumanChoice() function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getComputerChoice(){
-    const randn = Math.random()
-    console.log(randn)
+    const randn = Math.random();
     if(randn <= 0.3){
         return "rock";
     }else if(0.3 < randn && randn <= 0.6){
@@ -10,4 +9,15 @@ function getComputerChoice(){
     }else{
         return "Undefined value";
     }
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter your choice: ", "rock");
+    choice = choice.toLowerCase();
+    if(!(choice === 'rock' || choice ==='paper' || choice === 'scissor')){
+        if(confirm("IT'S EITHER ROCK OR PAPER OR SCISSOR HAVE YOU NEVER PLAYED BEFORE?!")){
+            choice = getHumanChoice();
+        } else return -1;
+    }
+    return choice;
 }
