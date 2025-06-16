c66199cfad1ee3ad2db123658fb3042e9e796428
saido <38206432+JSaii@users.noreply.github.com>
2025-06-16 14:18:30 +0900

    Fix round counter, retry logic, and add tie counter

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 1;
+let tieCounter = 0;
+
 const mapChoice = {
     'rock': 0,
     'paper': 1,
     'scissors': 2
 }
+
 const judgeMatrix = [
     [0, -1, 1],
     [1, 0, -1],
@@ -14,6 +17,7 @@ const judgeMatrix = [
 
 function getComputerChoice(){
     const randn = Math.random();
+
     if(randn <= 0.3){
         return "rock";
     }else if(0.3 < randn && randn <= 0.6){
@@ -26,7 +30,9 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(){
+
     while(true){
+
         let choice = prompt(
             `ROUND ${roundNumber}` + `\nScores: You - ${playerScore} CPU - ${computerScore}`+ "\n\nEnter your choice: ", 
             "rock"
@@ -47,45 +53,52 @@ function getPlayerChoice(){
             }
         }
     }
+
     return null;
 }
 
 function playRound(){
+
     let playerChoice = getPlayerChoice();
+
     if(playerChoice === null) {
         return null;
     }
+
     const computerChoice = getComputerChoice();
     const mappedPlayerChoice = mapChoice[playerChoice];
     const mappedComputerChoice = mapChoice[computerChoice];
-
     const gameStatusMsg = `You-> ${playerChoice} X ${computerChoice} <-CPU`;
-
     const judge = judgeMatrix[mappedPlayerChoice][mappedComputerChoice]
 
-    if(judge === 0){
+    if(judge === 0) {
+        tieCounter++;
         alert(gameStatusMsg + "\n\nTie")
-    }else if(judge === 1){
+    } else if(judge === 1) {
         playerScore++;
         alert(gameStatusMsg + "\n\nYou win !")
-    }else if(judge === -1){
+    } else if(judge === -1) {
         computerScore++;
         alert(gameStatusMsg + "\n\nYou lose !")
-    }else{
+    } else {
         alert("Undefined value")
     }
+
     return 1;
 }
 
 alert("You will play five rounds of rock paper scissors against the computer !")
 
 for(let i = 0;i < 5;i++){
+
     const round = playRound();
+
     if(round === null) {
         break;
     }
+
     roundNumber++;
 }
 
-alert(`Final score \n\nYou: ${playerScore}, CPU: ${computerScore}`)
+alert(`Rounds played : ${roundNumber-1}\nFinal score \n\nYou: ${playerScore}, CPU: ${computerScore}, Ties: ${tieCounter}`)
 alert('Refresh the page to play again.')
