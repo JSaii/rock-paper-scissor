2b3bffe74ac861fdf669b9ecdbfc49f73097669e
saido <38206432+JSaii@users.noreply.github.com>
2025-08-06 22:26:34 +0900

    Finish ui, unlimited gameplay mode

--- script.js
+++ script.js
@@ -1,104 +1,154 @@
-let playerScore = 0;
-let computerScore = 0;
-let roundNumber = 1;
-let tieCounter = 0;
 const ROCK_THRESHOLD = 0.3;
 const SCISSORS_THRESHOLD = 0.6;
-
+let currentPlayerSelection = null
+let stateObject = {
+    "playerChoice": null,
+    "cpuChoice": null,
+    "roundNumber": 1,
+    "playerScore": 0,
+    "cpuScore": 0,
+    "tieCounter": 0,
+    "roundWinner": null
+}
+const INITIAL_VALUES = {
+    "playerChoice": null,
+    "cpuChoice": null,
+    "roundNumber": 1,
+    "playerScore": 0,
+    "cpuScore": 0,
+    "tieCounter": 0,
+    "roundWinner": null
+}
 const mapChoice = {
     'rock': 0,
     'paper': 1,
     'scissors': 2
 }
-
 const judgeMatrix = [
     [0, -1, 1],
     [1, 0, -1],
     [-1, 1, 0]
 ]
 
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let cpuChosenButton = null
+
+async function stateManager(state){
+    if(state === "initial"){
+        stateObject = {...INITIAL_VALUES};
+        goButton.disabled = false;
+        choiceButtons.forEach(button => {
+            button.disabled = false
+            button.classList.remove("selected")
+            button.classList.remove("winner")
+            button.classList.remove("loser")
+        });
+        document.getElementById("player-score").textContent = stateObject.playerScore;
+        document.getElementById("cpu-score").textContent = stateObject.cpuScore;
+    } else if (state === "inter-round"){
+        goButton.disabled = true;
+        choiceButtons.forEach(button => button.disabled = true);
+        if(stateObject.roundWinner === "player"){
+            const selected = document.querySelector("button.selected");
+            selected.classList.add("winner");
+            selected.classList.remove("selected");
+        } else if(stateObject.roundWinner === "cpu"){
+            const selected = document.querySelector("button.selected");
+            selected.classList.add("loser");
+            selected.classList.remove("selected");
+        }
+        document.getElementById("player-score").textContent = stateObject.playerScore;
+        document.getElementById("cpu-score").textContent = stateObject.cpuScore;
+        await wait(2000);
+        stateManager("initial-round")
+    } else if (state === "initial-round"){
+        goButton.disabled = false;
+        choiceButtons.forEach(button => {
+            button.disabled = false
+            button.classList.remove("selected")
+            button.classList.remove("winner")
+            button.classList.remove("loser")
+        });
+
+        choiceButtonsCpu.forEach(button => {
+            button.disabled = false
+            button.classList.remove("selected")
+            button.classList.remove("winner")
+            button.classList.remove("loser")
+        });
+        stateObject.roundWinner = null;
+    }
+}
+
+const choiceButtons = document.querySelectorAll(".choice-button:not(.cpu)");
+const choiceButtonsCpu = document.querySelectorAll(".choice-button.cpu");
+const resetButton = document.getElementById("reset-bt")
+
+resetButton.addEventListener("click", () => {
+    stateManager("initial");
+})
+
+choiceButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        choiceButtons.forEach(button => button.classList.remove("selected"));
+        document.getElementById(button.id).classList.add("selected");
+        stateObject.playerChoice = button.id;
+        console.log(stateObject.playerChoice);
+    });
+});
+
+const goButton = document.getElementById("go-bt")
+goButton.addEventListener("click", () => {
+    playRound();
+})
+
 function getComputerChoice(){
     const randn = Math.random();
     
     if(randn <= ROCK_THRESHOLD) {
+        document.getElementById(`cpu-rock-bt`).classList.add("selected");
         return "rock";
     } else if(randn <= SCISSORS_THRESHOLD) {
+        document.getElementById(`cpu-paper-bt`).classList.add("selected");
         return "paper";
     } else {
+        document.getElementById(`cpu-scissors-bt`).classList.add("selected");
         return "scissors";
     }
-}
 
-function getPlayerChoice(){
-
-    while(true){
-
-        let choice = prompt(
-            `ROUND ${roundNumber}` + `\nScores: You - ${playerScore} CPU - ${computerScore}`+ "\n\nEnter your choice: ", 
-            "rock"
-        );
-
-        if(choice === null){
-            break;
-        }
-
-        choice = choice.toLowerCase();
-
-        if(choice === 'rock' || choice ==='paper' || choice === 'scissors') {
-            return choice;
-        } else {
-            const retry = confirm("IT'S EITHER ROCK OR PAPER OR SCISSORS HAVE YOU NEVER PLAYED BEFORE?!");
-            if(!retry) {
-                break;
-            }
-        }
-    }
-
-    return null;
 }
 
 function playRound(){
 
-    let playerChoice = getPlayerChoice();
+    let playerChoice = stateObject.playerChoice;
 
     if(playerChoice === null) {
+        alert("Please choose first!")
         return null;
     }
 
     const computerChoice = getComputerChoice();
     const mappedPlayerChoice = mapChoice[playerChoice];
     const mappedComputerChoice = mapChoice[computerChoice];
-    const gameStatusMsg = `You-> ${playerChoice} X ${computerChoice} <-CPU`;
     const judge = judgeMatrix[mappedPlayerChoice][mappedComputerChoice]
 
     if(judge === 0) {
-        tieCounter++;
-        alert(gameStatusMsg + "\n\nTie")
+        stateObject.tieCounter++;
+        stateManager("inter-round")
     } else if(judge === 1) {
-        playerScore++;
-        alert(gameStatusMsg + "\n\nYou win !")
+        stateObject.playerScore++;
+        stateObject.roundWinner = "player";
+        stateManager("inter-round")
     } else if(judge === -1) {
-        computerScore++;
-        alert(gameStatusMsg + "\n\nYou lose !")
+        stateObject.cpuScore++;
+        stateObject.roundWinner = "cpu";
+        stateManager("inter-round")
     } else {
         alert("Undefined value")
     }
 
     return 1;
 }
-
-alert("You will play five rounds of rock paper scissors against the computer !")
-
-for(let i = 0;i < 5;i++){
-
-    const round = playRound();
-
-    if(round === null) {
-        break;
-    }
-
-    roundNumber++;
-}
-
-alert(`Rounds played : ${roundNumber-1}\nFinal score \n\nYou: ${playerScore}, CPU: ${computerScore}, Ties: ${tieCounter}`)
-alert('Refresh the page to play again.')
